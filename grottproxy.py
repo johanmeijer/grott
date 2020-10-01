f65342636409d0a09537ac71c92e22d6db5a4c19
Johan Meijer <16508298+johanmeijer@users.noreply.github.com>
2020-10-01 17:04:49 +0200

    2.2.1c
    Some cosmetic and stability changes

--- grottproxy.py
+++ grottproxy.py
@@ -1,6 +1,7 @@
 #Grott Growatt monitor :  Proxy 
 #       
-# Updated: 2020-05-22
+# Updated: 2020-09-30
+# Version 2.2.1c
 
 import socket
 import select
@@ -10,17 +11,20 @@ import struct
 import textwrap
 from itertools import cycle # to support "cycling" the iterator
 import time, json, datetime, codecs
+## to resolve errno 32: broken pipe issue (only linux)
+if sys.platform != 'win32' :
+   from signal import signal, SIGPIPE, SIG_DFL
 
-from grottdata import procdata
+from grottdata import procdata, decrypt
 
 #import mqtt                       
 import paho.mqtt.publish as publish
 
 # Changing the buffer_size and delay, you can improve the speed and bandwidth.
 # But when buffer get to high or delay go too down, you can broke things
-#buffer_size = 4096
-buffer_size = 65535
-delay = 0.0001
+buffer_size = 4096
+#buffer_size = 65535
+delay = 0.0002
 
 class Forward:
     def __init__(self):
@@ -39,12 +43,24 @@ class Proxy:
     channel = {}
 
     def __init__(self, conf):
-        if conf.verbose: 
-            print("")
-            print("\nGrott proxy mode started\n")
+        print("\nGrott proxy mode started")
+        ## to resolve errno 32: broken pipe issue (Linux only)
+        if sys.platform != 'win32':
+            signal(SIGPIPE, SIG_DFL) 
+        ## 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #set default grottip address
+        if conf.grottip == "default" : conf.grottip = '0.0.0.0'
         self.server.bind((conf.grottip, conf.grottport))
+        #socket.gethostbyname(socket.gethostname())
+        try: 
+            hostname = (socket.gethostname())    
+            print("Hostname :", hostname)
+            print("IP : ", socket.gethostbyname(hostname), ", port : ", conf.grottport, "\n")
+        except:  
+            print("IP and port information not available") 
+
         self.server.listen(200)
         self.forward_to = (conf.growattip, conf.growattport)
         
@@ -58,7 +74,10 @@ class Proxy:
                 if self.s == self.server:
                     self.on_accept(conf)
                     break
-                self.data, self.addr = self.s.recvfrom(buffer_size)
+                try: 
+                    self.data, self.addr = self.s.recvfrom(buffer_size)
+                except: 
+                    if conf.verbose : print("\t - Grott connection error")    
                 if len(self.data) == 0:
                     self.on_close(conf)
                     break
@@ -81,7 +100,13 @@ class Proxy:
             clientsock.close()
 
     def on_close(self,conf):
-        if conf.verbose: print("\t -", self.s.getpeername(), "has disconnected")
+        if conf.verbose: 
+            #try / except to resolve errno 107: Transport endpoint is not connected 
+            try: 
+                print("\t -", self.s.getpeername(), "has disconnected")
+            except:  
+                print("\t -", "peer has disconnected")
+
         #remove objects from input_list
         self.input_list.remove(self.s)
         self.input_list.remove(self.channel[self.s])
@@ -99,6 +124,38 @@ class Proxy:
         print("")
         print("\t - " + "Growatt packet received:") 
         print("\t\t ", self.channel[self.s])
+        # FILTER!!!!!!!! Detect if configure data is sent!
+        header = "".join("{:02x}".format(n) for n in data[0:8])
+        if conf.blockcmd : 
+            #standard everything is blocked!
+            blockflag = True 
+            #partly block configure Shine commands                   
+            if header[14:16] == "18" :         
+                if conf.blockcmd : 
+                    if header[6:8] == "05" or header[6:8] == "06" : confdata = decrypt(data) 
+                    else :  confdata = data
+
+                    #get conf command (location depends on record type), maybe later more flexibility is needed
+                    if header[6:8] == "06" : confcmd = confdata[76:80]
+                    else: confcmd = confdata[36:40]
+                    
+                    if header[14:16] == "18" : 
+                        #do not block if configure time command of configure IP (if noipf flag set)
+                        if conf.verbose : print("\t - Grott: Shine Configure command detected")                                                    
+                        if confcmd == "001f" or (confcmd == "0011" and conf.noipf) : 
+                            blockflag = False
+                            if conf.verbose : print("\t - Grott: Configure time or IP address not blocked : ", confcmd)    
+                    else : 
+                        #All configure inverter commands will be blocked
+                        conf.verbose : print("\t - Grott: Inverter Configure command detected")
+            
+            #allow records: 
+            if header[12:16] in conf.recwl : blockflag = False     
+
+            if blockflag : 
+                print("\t - Grott: Record blocked: ", header[12:16])
+                return
+
         # send data to destination
         self.channel[self.s].send(data)
         if len(data) > conf.minrecl :
